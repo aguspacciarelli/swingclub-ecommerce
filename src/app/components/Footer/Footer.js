dc4d2cbf9463d0f84b96353693bec33ab56883e6
aguspacciarelli <agustina.pacciarelli@gmail.com>
2024-10-09 11:11:36 -0300

    prueba

--- src/app/components/Footer/Footer.js
+++ src/app/components/Footer/Footer.js
@@ -6,11 +6,11 @@ const Footer = () => {
   return (
     <footer className={styles["footer"]}>
         <div className={styles["icons"]}>
-            <Image src='/logo-blanco.svg' alt='Logo' width={100} height={100}/>
+            <Image src='/logo-blanco.svg' alt='Logo' width={100} height={100} className={styles["icono"]}/>
             <ul className={styles["redes"]}>
-              <li><Image src='/instagram.png' alt='Instagram' width={30} height={30}/></li>
-              <li><Image src='/twitter.png' alt='Twitter' width={30} height={30}/></li>
-              <li><Image src='/facebook.png' alt='Facebook' width={15} height={15}/></li>
+              <li><Image src='/instagram.png' alt='Instagram' width={30} height={30} className={styles["icono"]}/></li>
+              <li><Image src='/twitter.png' alt='Twitter' width={30} height={30} className={styles["icono"]}/></li>
+              <li><Image src='/facebook.png' alt='Facebook' width={15} height={15} className={styles["icono"]}/></li>
             </ul>
         </div>
         <div className={styles["list"]}>
